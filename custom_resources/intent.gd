5e1af67035846437d20d4f533b1ed6c1b9921333
NukkoD <149139683+NukkoD@users.noreply.github.com>
2024-05-25 01:25:37 -0700

    Finished Pt. 8, adding enemy intents, dynamic effects and small bug fixes

---
+++ custom_resources/intent.gd
@@ -0,0 +1,7 @@
+class_name Intent
+extends Resource
+
+@export var number: String
+@export var icon: Texture
+
+
--- scenes/card_ui/card_ui.gd
+++ scenes/card_ui/card_ui.gd
@@ -18,8 +18,8 @@ const HOVER_STYLEBOX:= preload("res://scenes/card_ui/card_hover_stylebox.tres")
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 @onready var targets: Array[Node] = []
-@onready var original_index := self.get_index()
 
+var original_index := 0
 var parent: Control
 var tween: Tween
 var playable := true : set = _set_playable # Flag for if card is playable for mana cost
